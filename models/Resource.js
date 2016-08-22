4fa5d396f1b5fa58a5c7ae49507a5d1c325c5b2c
jstockwin <jstockwin@gmail.com>
2016-08-22 20:46:27 +0100

    Add optional descriptions to links and resources

--- models/Resource.js
+++ models/Resource.js
@@ -6,6 +6,7 @@ var Resource = new keystone.List('Resource');
 Resource.add({
 	name: { type: String, required: true, index: true },
 	link: { type: Types.Url, initial: true, required: true, index: true },
+	description: { type: Types.Html, wysiwyg: true },
 });
 
 Resource.defaultColumns = 'name';
--- models/UsefulLink.js
+++ models/UsefulLink.js
@@ -6,6 +6,7 @@ var UsefulLink = new keystone.List('UsefulLink');
 UsefulLink.add({
 	name: { type: String, required: true, index: true },
 	link: { type: Types.Url, initial: true, required: true, index: true },
+	description: { type: Types.Html, wysiwyg: true },
 });
 
 UsefulLink.defaultColumns = 'name';
