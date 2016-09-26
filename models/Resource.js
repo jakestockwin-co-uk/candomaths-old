cfd825bee54d32d28e7e280d194162443cf05e8f
jstockwin <jstockwin@gmail.com>
2016-09-26 20:21:29 +0100

    Added displayOrder to links and resources

--- models/Resource.js
+++ models/Resource.js
@@ -7,7 +7,9 @@ Resource.add({
 	name: { type: Types.Text, required: true, index: true },
 	link: { type: Types.Url, initial: true, required: true, index: true },
 	description: { type: Types.Html, initial: true },
+	displayOrder: { type: Types.Number, initial: true },
 });
 
-Resource.defaultColumns = 'name';
+Resource.defaultColumns = 'name, displayOrder|20%';
+Resource.defaultSort = 'displayOrder';
 Resource.register();
--- models/UsefulLink.js
+++ models/UsefulLink.js
@@ -7,7 +7,9 @@ UsefulLink.add({
 	name: { type: Types.Text, required: true, index: true },
 	link: { type: Types.Url, initial: true, required: true, index: true },
 	description: { type: Types.Html, initial: true },
+	displayOrder: { type: Types.Number, initial: true },
 });
 
-UsefulLink.defaultColumns = 'name';
+UsefulLink.defaultColumns = 'name, displayOrder|20%';
+UsefulLink.defaultSort = 'displayOrder';
 UsefulLink.register();
